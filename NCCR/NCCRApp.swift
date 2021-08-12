0bcf1f436a270786ca84560845b4ade277c2ca10
Jack <finnis.j@etoncollege.org.uk>
2021-08-12 17:36:39 +0100

    Initial Commit

---
+++ NCCR/NCCRApp.swift
@@ -0,0 +1,17 @@
+//
+//  NCCRApp.swift
+//  NCCR
+//
+//  Created by Finnis on 12/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct NCCRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
